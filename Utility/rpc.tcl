f86f4684e4a28899efd41dd9ea8751ec34a0b45d
Nishi <nishi@nishi.boats>
2024-10-01 18:07:27 +0000

    rpc works
    git-svn-id: file:///raid/svn-personal/koakuma/trunk@5 219d0f9c-2d94-d447-890a-813e76b88fe9

--- Utility/rpc.tcl
+++ Utility/rpc.tcl
@@ -20,21 +20,21 @@ namespace eval rpc {
 	set username ""
 	set password ""
 	proc ask-auth {} {
-		upvar 1 username username
-		upvar 1 password password
+		global username
+		global password
 		puts -nonewline "Username: "
 		flush stdout
-		set username "[gets stdin]"
+		set ::rpc::username "[gets stdin]"
 		puts -nonewline "Password: "
 		flush stdout
 		exec stty -echo
-		set password "[gets stdin]"
+		set ::rpc::password "[gets stdin]"
 		exec stty echo
 		puts ""
 
 		set headers ""
 		lappend headers "Authorization"
-		lappend headers "Basic [::base64::encode -wrapchar "" "$username:$password"]"
+		lappend headers "Basic [::base64::encode -wrapchar "" "$::rpc::username:$::rpc::password"]"
 
 		global RPC_URL
 		set tok [::http::geturl "$RPC_URL" -headers $headers]
@@ -45,5 +45,20 @@ namespace eval rpc {
 			return 1
 		}
 		return 0
+	}
+	proc send {path content} {
+		set headers ""
+		global RPC_URL
+		if { "$::rpc::username" != "" } {
+			lappend headers "Authorization"
+			lappend headers "Basic [::base64::encode -wrapchar "" "$::rpc::username:$::rpc::password"]"
+		}
+		set tok [::http::geturl "$RPC_URL$path" -headers $headers -type "application/json" -query "$content"]
+		set code [::http::ncode $tok]
+		set body "[::http::data $tok]"
+		::http::cleanup $tok
+		lappend result "$code"
+		lappend result "$body"
+		return $result
 	}
 }
